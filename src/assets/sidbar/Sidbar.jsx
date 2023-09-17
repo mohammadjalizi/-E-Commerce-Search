a0c7cb7eb2e28df5fb19f3f4a8b80a85317f5acb
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-09-17 14:49:56 +0330

    Add sidbarcss

--- src/assets/sidbar/Sidbar.jsx
+++ src/assets/sidbar/Sidbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Sidbar.css'
 const Sidbar = () => {
   return (
-    <div>Sidbar</div>
+    <div>
+
+<section className="sidebar">
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+<h1>Category</h1>
+  <h1> Price</h1>
+       <h2>Colors</h2>
+      </section>
+    </div>
   )
 }
 
