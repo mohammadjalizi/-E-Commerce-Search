7af0f6b9c5892d32214583797f70dfa4c3c8d18a
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-09-17 15:18:31 +0330

    Add file price

---
+++ src/assets/sidbar/price/Price.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const Price = () => {
+  return (
+    <div>
+
+<div>
+
+<h2 className="sidebar-title price-title">Price</h2>
+
+
+
+<div>
+
+<label className="sidebar-label-container">
+          <input  type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+  
+        <label className="sidebar-label-container">
+          <input  type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+  
+        <label className="sidebar-label-container">
+          <input  type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+  
+        <label className="sidebar-label-container">
+          <input  type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+  
+        <label className="sidebar-label-container">
+          <input  type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+  
+</div>
+</div>
+       
+        
+        
+    </div>
+  )
+}
